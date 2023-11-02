baf5711d22a995f1da81016017c92a66883b764f
Rohit_Kakade <rohit08kakade@gmail.com>
2023-11-02 12:01:52 +0530

    First Commit

---
+++ git-learning/src/main/java/Selenium_Practice/git_learning/App.java
@@ -0,0 +1,13 @@
+package Selenium_Practice.git_learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
